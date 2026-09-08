8a14157855e8a0b46d482af3da43fae45073aa00
nukeador <nukeador@protonmail.com>
2026-09-08 18:31:10 +0200

    Refresh weather cache hourly

--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -1,6 +1,6 @@
 const WEATHER_API_URL = 'https://api.aldeapucela.org/weather';
 const WEATHER_CACHE_KEY = 'fiestas-valladolid-weather-v3';
-const WEATHER_CACHE_TTL_MS = 6 * 60 * 60 * 1000;
+const WEATHER_CACHE_TTL_MS = 60 * 60 * 1000;
 const WEATHER_TIMEOUT_MS = 5000;
 
 const weatherConditions = [
